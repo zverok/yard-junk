f6888e5fca15e685692ca1f9fd3c21fb8a6e3e9a
zverok <zverok.offline@gmail.com>
2017-08-27 19:17:49 +0300

    Here we go

--- lib/junk_yard/rake.rb
+++ lib/junk_yard/rake.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 module JunkYard
   module Rake
     extend ::Rake::DSL
 
     def self.define_task
-      desc "Check th junk in your YARD Documentation"
+      desc 'Check th junk in your YARD Documentation'
       task('yard:junk') do
         require 'yard'
         require_relative '../junk_yard'
